93640aed104c053ca292aaad1bd9a82dd65d9873
brentlad <54677922+brentlad@users.noreply.github.com>
2019-09-22 13:14:40 -1200

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app  = main.app;
 app.use(function(request,response,next)
 {
 	console.log(request.headers['x-forwarded-proto']);
+	if(request.headers['x-forwarded-proto'])
+	{
+		res.redirect('https://' + req.headers.host + req.url);
+	}
 	next();
 })
 const server = require('./node_modules/server.js');
