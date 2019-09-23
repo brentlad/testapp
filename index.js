5d5d2adfbfad018213f5a411379a4d1710430f8e
brentlad <54677922+brentlad@users.noreply.github.com>
2019-09-22 13:01:46 -1200

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const main = require('./node_modules/main.js');
+const app  = main.app;
+app.use(function(request,response,next)
+{
+	console.log(request.protocol);
+	next();
+})
 const server = require('./node_modules/server.js');
 const homeRoute = require('./routes/home.js');
