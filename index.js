fd5d51fca082657c39497757cb5d3909ca406fd5
brentlad <54677922+brentlad@users.noreply.github.com>
2019-09-22 13:35:24 -1200

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const main = require('./node_modules/main.js');
 const app  = main.app;
 app.use(function(request,response,next)
 {
-	console.log(request.headers['x-forwarded-proto']);
+	//console.log(request.headers['x-forwarded-proto']);
+	if(request.headers['x-forwarded-proto'] !== "https")
+	{
+		 response.redirect('https://' + request.headers.host + request.url);
+	}
 	next();
 })
 const server = require('./node_modules/server.js');
